c2f8bb79e35de6cefeb0e7e62d968758861d3f29
rwart <32432397+rwart@users.noreply.github.com>
2017-11-28 02:23:12 +0100

    now with modules ready for review

--- modules/OSInfo.js
+++ modules/OSInfo.js
@@ -13,22 +13,26 @@ module.exports = {
    *  CPU model, Uptime, User name, Home dir
    */
   print: function () {
-    var type = os.type();
-    if (type === 'Darwin') {
-      type = 'OSX';
-    } else if (type === 'Windows_NT') {
-      type = 'Windows';
-    }
-
-    var release = os.release();
-    var cpu = os.cpus()[0].model;
-    var uptime = os.uptime(); // in seconds
-    var userInfo = os.userInfo();
-    console.log('System:', type);
-    console.log('Release:', release);
-    console.log('CPU model:', cpu);
-    console.log('Uptime: ~', timeFormat.toHMSinPolish(uptime));
-    console.log('User name:', userInfo.username);
-    console.log('Home dir:', userInfo.homedir);
+    getOSinfo();
   },
 };
+
+function getOSinfo() {
+  var type = os.type();
+  if (type === 'Darwin') {
+    type = 'OSX';
+  } else if (type === 'Windows_NT') {
+    type = 'Windows';
+  }
+
+  var release = os.release();
+  var cpu = os.cpus()[0].model;
+  var uptime = os.uptime(); // in seconds
+  var userInfo = os.userInfo();
+  console.log('System:', type);
+  console.log('Release:', release);
+  console.log('CPU model:', cpu);
+  console.log('Uptime: ~', timeFormat.toHMSinPolish(uptime));
+  console.log('User name:', userInfo.username);
+  console.log('Home dir:', userInfo.homedir);
+}
